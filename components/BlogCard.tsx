c07c569ea1db7359f09262a87e5bbe21eaa23b9d
Alex Martinig <alex.martinig@calibre.org.uk>
2023-06-06 16:32:00 +0100

    Creating and connecting links to pages

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -1,16 +1,22 @@
+import Link from "next/link";
 import { FC } from "react";
 
 interface Props {
-    title: string;
-    desc: string;
+  title: string;
+  desc: string;
+  slug: string;
 }
 
-const BlogCard: FC<Props> = ({ title, desc }): JSX.Element => {
+const BlogCard: FC<Props> = ({ title, desc, slug }): JSX.Element => {
   return (
-    <div className="bg-green-100 p-2 rounded">
-      <h1 className="text-3xl text-gray-900 font-semibold">{title}</h1>
-      <p className="text-gray-500">{desc}</p>
-    </div>
+    <Link legacyBehavior href={"/blogs/" + slug}>
+      <a className="block">
+        <div className="bg-green-100 p-2 rounded cursor-pointer">
+          <h1 className="text-gray-900 text-3xl font-semibold">{title}</h1>
+          <p className="text-gray-500">{desc}</p>
+        </div>
+      </a>
+    </Link>
   );
 };
 
--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,14 +1,38 @@
+import { InferGetStaticPropsType, NextPage } from "next";
+import { useState, useEffect } from "react";
 import BlogCard from "@/components/BlogCard";
-import { NextPage } from "next";
 
-interface Props {}
+interface PostApiResponse {
+  postInfo: {
+    title: string;
+    slug: string;
+    meta: string;
+  }[];
+}
 
-const Blogs: NextPage<Props> = () => {
+export const getStaticProps = async () => {
+  const { postInfo }: PostApiResponse = await fetch(
+    "http://localhost:3000/api/posts"
+  ).then((data) => data.json());
+
+  return {
+    props: { posts: postInfo },
+  };
+};
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+const Blogs: NextPage<Props> = ({ posts }) => {
   return (
     <div className="max-w-3xl mx-auto p-5 space-y-5">
-      <BlogCard title="This is my blog" desc="This is my description" />
-      <BlogCard title="This is my blog" desc="This is my description" />
-      <BlogCard title="This is my blog" desc="This is my description" />
+      {posts.map((post) => (
+        <BlogCard
+          key={post.slug}
+          title={post.title}
+          desc={post.meta}
+          slug={post.slug}
+        />
+      ))}
     </div>
   );
 };
